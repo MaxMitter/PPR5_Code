e62e31a13d330d2af511f2d65e9199f27f2fdb3f
Max Mitter <max.mitter14@gmail.com>
2021-11-09 21:58:02 +0100

    Done Docu for HW3

--- HW3/Result_Analysis.py
+++ HW3/Result_Analysis.py
@@ -1,7 +1,7 @@
 import HW1.basic_library as walk_generator
 import statistics
 
-walks = walk_generator.monte_carlo_walk_analysis(50, 10_0)
+walks = walk_generator.monte_carlo_walk_analysis(50, 10_000)
 
 len4Roundtrips = [w for w, dist in walks[4] if dist == 0]
 
@@ -9,13 +9,16 @@ print(len(len4Roundtrips))
 
 allRoundTrips = {k: [w for w, dist in walks[k] if dist == 0] for k in walks}
 uniqueRoundTrips = {k: {tuple(w) for w in allRoundTrips[k]} for k in allRoundTrips}
-first10RoundTrips = {k: [w for w in allRoundTrips[k][:10]] for k in allRoundTrips}
-print(allRoundTrips)
+first10RoundTrips = {k: [w for w in list(uniqueRoundTrips[k])[:10]] for k in uniqueRoundTrips}
+
+print(len(uniqueRoundTrips))
+print(first10RoundTrips)
 
 
 def calculate_mean(walk):
     dist = [dist for w, dist in walk]
     return statistics.mean(dist)
+
 
 def calculate_median(walk):
     dist = [dist for w, dist in walk]
@@ -46,18 +49,16 @@ print("Median: " + str(calculate_median(walks[25])))
 maxPossibleDist = {k: [(w, dist) for w, dist in walks[k] if dist == k] for k in walks}
 maxPossibleDistPercentage = {k: (len(maxPossibleDist[k]) / len(walks[k]) * 100) for k in walks}
 
+print(maxPossibleDistPercentage)
+
 def checkEqual(iterator):
     if len(iterator) == 0:
         return True
-
     for x in iterator:
         if x != iterator[0]:
             return False
-
     return True
 
 
 allStraightTrips = {k: [w for w, dist in walks[k] if checkEqual(w)] for k in walks}
 uniqueStraightTrips = {k: {tuple(w) for w in allStraightTrips[k]} for k in allStraightTrips}
-
-print()
